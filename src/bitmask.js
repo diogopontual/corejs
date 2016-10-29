2652c6098bda513a8eb7565b9035b4880d1635d0
Diogo <diogopontual@gmail.com>
2016-10-29 00:39:37 -0200

    bitmask improvements

--- src/bitmask.js
+++ src/bitmask.js
@@ -1,36 +1,46 @@
+/*************************************************************************
+ * @description
+ * A bitmask utility. Can hold the true or false state of up to 31 variables;
+ * 
+ * @author
+ * Diogo Dauster Pontual (diogopontual@gmail.com)
+ *
+ *************************************************************************/
 const bitmask = function () {
     if (arguments.length > 31)
         throw "The maximum number of constants is 31";
     let ret = Object.create(bitmaskProto);
-    ret.state = 0;
-    ret.variables = {};
+    ret._state = 0;
+    ret._variables = {};
     for (let i = 0; i < arguments.length; i++) {
-        ret.variables[arguments[i]] = i;
+        ret._variables[arguments[i]] = i;
     }
-    ret.__proto__ = BitmaskPrototype;
     return ret;
 };
 const bitmaskProto = {
     set: function () {
         for (let i = 0; i < arguments.length; i++)
-            this.state = this.state |= 1 << this.variables[arguments[i]];
+            this._state = this._state |= 1 << this._variables[arguments[i]];
+        return this;
     },
     unset: function () {
         for (let i = 0; i < arguments.length; i++)
-            this.state = this.state &= ~(1 << this.variables[arguments[i]]);
+            this._state = this._state &= ~(1 << this._variables[arguments[i]]);
+        return this;
     },
     toggle: function () {
         for (let i = 0; i < arguments.length; i++)
-            this.state = this.state ^= 1 << this.variables[arguments[i]];
+            this._state = this._state ^= 1 << this._variables[arguments[i]];
+        return this;
     },
     test: function (variable) {
         if (!variable)
             return this.list();
-        return ((this.state >> this.variables[variable]) & 1) == 1;
+        return ((this._state >> this._variables[variable]) & 1) == 1;
     },
-    list: function () {
+    state: function () {
         let ret = {};
-        for (let v in this.variables) {
+        for (let v in this._variables) {
             ret[v] = this.test(v);
         }
         return ret;
