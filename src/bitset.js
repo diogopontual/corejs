8341915370b28e28d4fd476df3b316cc59a135e9
Diogo Dauster Pontual <diogopontual@MacBook-Air-de-Paula.local>
2016-10-31 14:30:40 -0200

    just to continue on other computer

--- src/bitset.js
+++ src/bitset.js
@@ -15,7 +15,7 @@ const bitset = function (length) {
 }
 const bitsetProto = {
     get: function (idx) {
-        return (this.arr[Math.trunc(idx / 8)] & 1 << (idx % 8));
+        return (this.arr[Math.trunc(idx / 8)] & 1 << (idx % 8)) > 0;
     },
     set: function (idx) {
         this.arr[Math.trunc(idx / 8)] |= 1 << (idx % 8);
@@ -25,12 +25,37 @@ const bitsetProto = {
         this.arr[Math.trunc(idx / 8)] &= 1 << ~(idx % 8);
         return this;
     },
-    compress: function (wordLength = 4) {},
-    toString: function(){
-        let r = '', z = 0, o = 0;
+    compress: function (zeroLength = 4) {
+        let ret = []
+        let oneLength = 8 - zeroLength, zeroMax = Math.pow(2,zeroLength) - 1, oneMax = Math.pow(2,oneLength) - 1, current = this.get(0), last = current, count = 0, byte = 0;
+        for (let i = 0; i < this.length; i++) {
+            current = this.get(i);
+            console.log(current);
+            if(current != last){
+                if(last){
+                    count = count << zeroLength;
+                }
+                byte |= count;
+                ret.push(byte);
+                count = 0;
+            }
+            count++;
+            current = last;
+        } 
+        ret.push(byte);
+        return ret;
+    },
+    toString: function () {
+        let r = '';
         for (let i = 0; i < this.length; i++) {
             r += this.get(i) ? 1 : 0;
         }
         return r;
     }
 };
+
+let b = bitset(16);
+b.set(7)
+b.unset(7)
+b.set(15)
+console.log(b.compress());
